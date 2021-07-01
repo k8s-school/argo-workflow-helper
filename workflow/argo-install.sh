3068b54f75666995b0ec021d4039e4f5810ba7a1
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2021-07-01 12:23:26 +0200

    Install argo workflow inside current namespace

--- workflow/argo-install.sh
+++ workflow/argo-install.sh
@@ -1,32 +1,38 @@
 #!/bin/bash
 
+# Install argo workflow using helm
+
 # @author: Fabrice Jammes, IN2P3
 set -euxo pipefail
 
 DIR=$(cd "$(dirname "$0")"; pwd -P)
 
-NS="argo"
+NS=$(kubectl get sa -o=jsonpath='{.items[0].metadata.namespace}')
 
-echo "Install Argo Workflow inside k8s"
+echo "Install Argo Workflow inside namespace $NS"
 helm repo add argo https://argoproj.github.io/argo-helm --force-update
 helm repo update
 helm install --create-namespace --namespace "$NS" -f "$DIR/values.yaml" argo-workflows argo/argo-workflows --version 0.2.6
 
-cat <<EOF | kubectl apply -f -
-apiVersion: rbac.authorization.k8s.io/v1
-kind: RoleBinding
-metadata:
-  name: argo
-  namespace: $NS
-roleRef:
-  apiGroup: rbac.authorization.k8s.io
-  kind: ClusterRole
-  name: edit
-subjects:
-- kind: ServiceAccount
-  name: default
-  namespace: $NS
-EOF
+# cat <<EOF | kubectl apply -f -
+# apiVersion: rbac.authorization.k8s.io/v1
+# kind: RoleBinding
+# metadata:
+#   name: argo
+#   namespace: $NS
+# roleRef:
+#   apiGroup: rbac.authorization.k8s.io
+#   kind: ClusterRole
+#   name: edit
+# subjects:
+# - kind: ServiceAccount
+#   name: argo-workflow
+#   namespace: $NS
+# EOF
+
+echo "Add additional RBAC to  argo-workflow service account (manage jobs.batch)"
+kubectl patch roles.rbac.authorization.k8s.io argo-workflows-workflow --type='json' \
+    -p='[{"op": "add", "path": "/rules/-", "value": {"apiGroups": ["batch"],"resources": ["jobs"],"verbs": ["create", "get", "watch"]} }]'
 
 kubectl wait --namespace "$NS" --for=condition=available --timeout=600s deployment argo-workflows-server argo-workflows-workflow-controller
 
