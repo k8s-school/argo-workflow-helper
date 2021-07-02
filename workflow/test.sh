7767df28732a8fce27be402109f85195b20ae28d
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2021-07-02 08:40:47 +0200

    Fix ci

--- workflow/test.sh
+++ workflow/test.sh
@@ -5,5 +5,7 @@
 # @author: Fabrice Jammes, IN2P3
 set -euxo pipefail
 
+NS=$(kubectl get sa -o=jsonpath='{.items[0].metadata.namespace}')
+
 argo submit -n "$NS" --serviceaccount=argo-workflow \
   https://raw.githubusercontent.com/argoproj/argo-workflows/master/examples/hello-world.yaml --watch
